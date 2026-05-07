f8f7c4f371058a2ce558a0f4e2993c0d1970566f
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2026-05-07 12:56:02 -0500

    fix: set default value for PORT in envs configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export const envs = {
-    PORT: get('PORT').required().asPortNumber(),
+    PORT: get('PORT').default('3000').asPortNumber(),
     JWT_SEED: get('JWT_SEED').required().asString(),
     MONGO_URL: get('MONGO_URL').required().asString(),
     MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
